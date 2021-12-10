b75a1bd235d42d02c1b8855913072edbce4ceba9
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-12-10 12:30:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+float funzione (const float a[], int N){
+    float max = a[0];
+    for (int i=0; i<N; i++){
+        if (a[i]> max ){
+            max= a[i];
+        }
+    }return max;
+}
+int main () {
+    int dim;
+    cin>> dim;
+    float vett[dim];
+    for (int i=0; i<dim; i++){
+        cin >> vett[i];
+    }
+    float max;
+    max = funzione(vett, dim);
+    cout << max << endl;
+
+    return 0;
 }
